a4b07eb007603b90e1ec0d8f5bb16475d68ed305
Lian Mafutra <lianmafutra@gmail.com>
2023-05-27 13:48:49 +0700

    first

---
+++ src/view/select2.blade.php
@@ -0,0 +1,14 @@
+<div class="form-group">
+   <label> {{ $label }}
+       @if ($required == 'true')
+           <span style="color: red">*</span>
+       @endif
+   </label>
+   <select id="{{ $id }}" name="{{ $id }}" @if ($required == 'true') required @endif
+    class="select2 select2-{{ $id }} form-control select2bs4"
+       data-placeholder="{{ $placeholder }}" style="width: 100%;">
+       <option></option>
+       {{ $slot }}
+   </select>
+   <span class="text-danger error-text {{ $id }}_err"></span>
+</div>
